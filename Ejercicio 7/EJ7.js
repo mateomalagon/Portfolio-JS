420d32aea627525a202f562823dc0816be7d919a
mateomalagon <mateo.malagon.fp@iescampanillas.com>
2022-05-09 14:26:14 +0200

    EJ7 Completo (todavia  ningún ejercicio tiene CSS terminado)

--- Ejercicio 7/EJ7.js
+++ Ejercicio 7/EJ7.js
@@ -3,3 +3,18 @@
 JS del Ejercicio 7, División por Euclides
 
 */
+
+function divisionEuclides (){
+
+  let divi = Number(document.getElementById('dividendo').value);
+  let disor = Number(document.getElementById('divisor').value);
+  let cociente = 0;
+
+  while (divi>=disor){
+
+    divi -= disor;
+    cociente++;
+  }
+
+  document.getElementById('resultadoDivision').innerHTML = "El cociente es <strong>" + cociente + "</strong> y el resto <strong>" + divi + "</strong>";
+}
